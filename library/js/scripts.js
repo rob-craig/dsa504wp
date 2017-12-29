09786d9a5df5e9cabd9223f4c768494cc95da2ae
azdak <negatia@gmail.com>
2017-12-29 12:11:26 -0600

    Cleaned out some legacy code

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,41 +1,5 @@
 
 jQuery(document).ready(function($) {
-	
-	jQuery('a[href="tel:5045175529"]').click(function(){
-		ga('send', {
-		  hitType: 'event',
-		  eventCategory: 'Button',
-		  eventAction: 'ctc',
-		  eventLabel: 'Clicked to Call'
-		})
-	});
-	
-	var vMiddle;
-	var headlineHeight;
-	
-	//jQuery( window ).load(function() {
-		
-		if(jQuery( window ).width()>768){
-			vMiddle = jQuery('.home .hero-section').height();
-			headlineHeight = jQuery('.home .full-bleed-hero-headline').height();
-			console.log(headlineHeight);
-			console.log(vMiddle);
-			jQuery('.home .full-bleed-hero-headline').css({ 'margin-top': -(vMiddle/2)-headlineHeight/2+'px' });
-		};
-		
-		//window.dispatchEvent(new Event('resize'));
-	//});
-	
-	jQuery( window ).resize(function() {
-		vMiddle = jQuery('.home .hero-section').height();
-		headlineHeight = jQuery('.home .full-bleed-hero-headline').height();
-		if(jQuery( window ).width()>768){
-			jQuery('.home .full-bleed-hero-headline').css({ 'margin-top': -(vMiddle/2)-headlineHeight/2+'px' });
-		}
-		//jQuery('.full-bleed-hero-introbox').css({ 'margin-top': -(vMiddle/2)-targetHeight/2+'px' });
-	});
-
-	
 
   jQuery('.material-design-hamburger__icon').click(
     function() {      
