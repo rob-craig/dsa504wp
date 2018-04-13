0ca0fe9f1f71f0787c33bc2cb493673637c8e03d
Mark D'Arensbourg <mdarens@gmail.com>
2018-04-13 13:06:15 -0500

    point to react app

--- addons/calendar-simple.php
+++ addons/calendar-simple.php
@@ -46,7 +46,5 @@
     <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
     <!-- Calendar-specific stuff -->
     <!-- <link rel="stylesheet" type="text/css" media="screen" href="/css/events.css" /> -->
-    <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/manifest.2ae2e69a05c33dfc65f8.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/vendor.6680367ded8e1c0ca3f0.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/app.6e573df67ed86dccc1f3.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/main.8465b917.js"></script>
     
