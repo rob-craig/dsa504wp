1d542683b2274ab4c9e01357968b42ac4215d17f
Trey Daniel <negatia@gmail.com>
2018-04-13 18:13:52 -0500

    Reverted due to calendar death

--- page-events.php
+++ page-events.php
@@ -60,8 +60,12 @@
     <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>   
     <!-- Calendar-specific stuff -->
     <!-- <link rel="stylesheet" type="text/css" media="screen" href="/css/events.css" /> -->
+	<!--
     <script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/main.6bf9d8ec.js"></script>
-    
+    -->
+	<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/manifest.2ae2e69a05c33dfc65f8.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/vendor.6680367ded8e1c0ca3f0.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/app.6e573df67ed86dccc1f3.js"></script>
 
 
 						</div>
