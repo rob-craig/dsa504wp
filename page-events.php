07b917998bbd450cf695d101519c734feeba95b4
Trey Daniel <negatia@gmail.com>
2018-08-22 19:44:11 -0500

    Testing new react calendar on main Events page

--- page-events.php
+++ page-events.php
@@ -16,13 +16,14 @@
 
 							<div id="dsa-cal-app" v-cloak>
 							  <?php 
-							  $limitNumEvents = false;
-							  include("addons/calendar-og.php"); ?>
+							  //$limitNumEvents = false;
+							  //include("addons/calendar-og.php"); ?>
 							</div>
-
+							<!-- new react calendar -->
+							<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/library/js/main.a60ae860.js"></script>
 							<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>   
 							<!-- Calendar-specific stuff -->
-							<?php include("addons/calendar-og-scripts.php"); ?>
+							<?php //include("addons/calendar-og-scripts.php"); ?>
 
 
 						</div>
